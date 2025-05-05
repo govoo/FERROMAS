5b31f200f719ccfc5fa46c8718b529a6458212a3
govoo <ga.godoyl@duocuc.cl>
2025-05-05 09:41:59 -0400

    logo

--- ferromax/src/pages/home.js
+++ ferromax/src/pages/home.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+
 function home() {
   return (
     <div class="text-center">
-        home
+        <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"></img>
+                <label>Bosstrap</label>
+            </a>
+        </div>
+        </nav>
     </div>
   )
 }
