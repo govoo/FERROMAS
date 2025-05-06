e87e8b541dc458898834d9cbf7a1c0b04ee51c75
Gavoo0 <gabrieli.godoyl@gmail.com>
2025-05-06 17:23:43 -0400

    Logo

--- ferromax/src/pages/home.js
+++ ferromax/src/pages/home.js
@@ -1,14 +1,13 @@
 import React from 'react'
-
+import ferromasLogo from '../img/ferromas_logo.svg';
 
 function home() {
   return (
     <div class="text-center">
         <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"></img>
-                <label>Bosstrap</label>
+            <a class="navbar-brand" href="/">
+                <img src={ferromasLogo} alt="Logo" width="50" height="50" class="d-inline-block align-text-top"></img>
             </a>
         </div>
         </nav>
